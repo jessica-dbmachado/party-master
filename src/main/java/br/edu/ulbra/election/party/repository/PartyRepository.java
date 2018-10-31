5ca5da121ffae8e4239846db3c292959f69a32a2
jessica-dbmachado <44639567+jessica-dbmachado@users.noreply.github.com>
2018-10-31 13:13:27 -0300

    Update src/main/java/br/edu/ulbra/election/party/repository/PartyRepository.java

---
+++ src/main/java/br/edu/ulbra/election/party/repository/PartyRepository.java
@@ -0,0 +1,7 @@
+package br.edu.ulbra.election.party.repository;
+
+import br.edu.ulbra.election.party.model.Party;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PartyRepository extends CrudRepository<Party, Long> {
+}
