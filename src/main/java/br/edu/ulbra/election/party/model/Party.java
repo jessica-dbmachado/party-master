626c79008ca4003746d7b71f11193f0a2d6452ab
jessica-dbmachado <44639567+jessica-dbmachado@users.noreply.github.com>
2018-10-31 12:22:28 -0300

    Update src/main/java/br/edu/ulbra/election/party/model/Party.java

---
+++ src/main/java/br/edu/ulbra/election/party/model/Party.java
@@ -0,0 +1,52 @@
+package br.edu.ulbra.election.Party.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Party {
+//string code // string name // intger number
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer number;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+}
